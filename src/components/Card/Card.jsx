403ee6b3cf3ea0750878fc8c3b2e1c44bf86572f
the Viper Kailash Suthar <ksuthar937@gmail.com>
2023-09-21 16:14:18 +0530

    muiActivity-tooltip-qtify-takehome

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,4 +1,5 @@
 import { Chip } from "@mui/material";
+import Tooltip from "@mui/material/Tooltip";
 
 import styles from "./Card.module.css";
 
@@ -6,23 +7,25 @@ function Card({ data, type }) {
   const getCart = (type) => {
     switch (type) {
       case "album": {
-        const { image, follows, title } = data;
+        const { image, follows, title, songs } = data;
         return (
-          <div className={styles.wrapper}>
-            <div className={styles.card}>
-              <img src={image} alt="album" />
-              <div className={styles.banner}>
-                <Chip
-                  className={styles.chip}
-                  label={`${follows} Follows`}
-                  size="small"
-                />
+          <Tooltip title={`${songs.length} songs`} placement="top" arrow>
+            <div className={styles.wrapper}>
+              <div className={styles.card}>
+                <img src={image} alt="album" />
+                <div className={styles.banner}>
+                  <Chip
+                    className={styles.chip}
+                    label={`${follows} Follows`}
+                    size="small"
+                  />
+                </div>
+              </div>
+              <div className={styles.title}>
+                <p>{title}</p>
               </div>
             </div>
-            <div className={styles.title}>
-              <p>{title}</p>
-            </div>
-          </div>
+          </Tooltip>
         );
       }
       case "song": {
